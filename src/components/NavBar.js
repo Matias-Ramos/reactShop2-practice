43cdeea7057477fd1d61db1dbcbdf2f8db61faf9
Matias <mattsk97@gmail.com>
2022-09-07 22:09:43 -0300

    Creado NavBar del sitio con Bootstrap

---
+++ src/components/NavBar.js
@@ -0,0 +1,21 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function NavBar() {
+    return (
+      <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">TiendaMia</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Contacto</Nav.Link>
+            <Nav.Link href="#features">Faq</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      </>
+  );
+}
+
+export default NavBar;
