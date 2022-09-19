cc7c9aa3409d0db58bd269ae30f987be55966d59
Matias <mattsk97@gmail.com>
2022-09-19 11:41:40 -0300

    Mejorados detalles de indentacion y comentarios

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,18 +1,19 @@
-//Presentational-component
+//Componente de presentación
 import Item from "./Item";
 
-//Funciones modificadoras
+//Funciones modificadoras del ItemCount.jsx
 let onAdd = (desiredAmount,setDesiredAmount, stock) => desiredAmount<stock && setDesiredAmount(desiredAmount+1);
 let onSubtract = (desiredAmount, setDesiredAmount) => desiredAmount>1 && setDesiredAmount(desiredAmount-1);
 
-//Inmutabilidad para el inicializador
+//Inmutabilidad para el inicializador para ItemCount.jsx
 const itemCountInitializer = 1;
   
 
 function ItemList( {productList} ) {
     return (
         <>
-        { productList.map( item => 
+        {
+            productList.map( item => 
             <Item 
              key={item.id}
              product_name={item.product_name}
@@ -21,11 +22,10 @@ function ItemList( {productList} ) {
              stock={item.stock}
              onAdd={onAdd}
              onSubtract={onSubtract}
-             itemCountInitializer={itemCountInitializer}
-            />
-             )}
+             itemCountInitializer={itemCountInitializer}/> )
+        }
         </>
-  )
+    )
 }
 
 export default ItemList;
